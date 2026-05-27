ec96e90ee4ce571e89836e80094c94f88668d9d0
prudhvi <prudhvi4u.s.com>
2026-05-27 12:53:26 +0530

    launching instance

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z07086101C1CVP7AT2UK4" # replace with your zone ID
-DOMAIN_NAME="daws90s.shop" # replace with your domain name
+ZONE_ID="Z00870693UTAC7ZXSBNIR" # replace with your zone ID
+DOMAIN_NAME="jhlittles.online" # replace with your domain name
 
 for instance in $@
 do
