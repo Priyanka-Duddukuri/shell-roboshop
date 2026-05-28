775b3ea099e332980625d20214167919248a1891
prudhvi <prudhvi4u.s.com>
2026-05-28 14:41:22 +0530

    v2

---
+++ roboshop-v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z00870693UTAC7ZXSBNIR" # replace with your zone ID
+DOMAIN_NAME="jhlittles.online" # replace with your domain name
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+### Validation ###
+if [ $# -lt 2 ]; then
+    echo -e "$R ERROR:: Atleast 2 arguments required $N"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
