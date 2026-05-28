d9ecd8326ae60f755df425ef3cb9e69c2f0f85c4
prudhvi <prudhvi4u.s.com>
2026-05-28 14:32:08 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z00870693UTAC7ZXSBNIR" # replace with your zone ID
 DOMAIN_NAME="jhlittles.online" # replace with your domain name
 
+
 for instance in $@
 do
     echo "Launching instance: $instance"
@@ -15,7 +16,6 @@ do
         --query 'Instances[0].InstanceId' \
         --output text
     )
-
     echo "Instance ID: $INSTANCE_ID"
 
     if [ $instance == "frontend" ]; then
@@ -28,17 +28,14 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
          --query 'Reservations[*].Instances[*].PrivateIpAddress' \
          --output text
-             )
-
+        )
         R53_RECORD="$instance.$DOMAIN_NAME"
     fi
     
     #### Updating R53 Record ####
-    (aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch 
-    )
-    '
+    --change-batch '
         {
             "Comment": "Update A record to new IP",
             "Changes": [
